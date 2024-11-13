195f0edd916bf29c3e3bbd2e2c7724235788d623
fahdiazhannu31 <fahdiazhannu31@gmail.com>
2024-11-13 15:34:58 +0700

    update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,27 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+
+// Route untuk halaman utama
+$routes->group('', ['filter' => 'login'], function ($routes) {
+    $routes->get('/', 'Users::index');
+    $routes->get('/aboutus', 'Users::aboutus');
+});
+
+// Grup routes untuk `Users`
+$routes->group('', ['filter' => 'role:users'], function ($routes) {
+    $routes->get('/listpackage', 'Users::listpackage');
+    $routes->get('/detailpackage', 'Users::detailpackage');
+});
+
+// Grup routes untuk `Admin`
+$routes->group('', ['filter' => 'role:admin'], function ($routes) {
+    $routes->get('/admin', 'Admin::index');
+    $routes->get('/admin/(:num)', 'Admin::detailuser/$1');
+    // $routes->get('/Admin/index', 'Admin::index');
+});
+
+// Routes untuk Payment dan API lainnya
 $routes->post('/payments', 'PaymentController::create');
 $routes->post('/payments/webhook/xendit', 'PaymentController::webhook');
 $routes->get('/api/routes', 'Route::getRoutes');
